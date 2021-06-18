6700d4505c67a1aa8d84e7a26f229652ce83538d
unknown <vikichet@gmail.com>
2021-06-19 03:38:05 +0530

    added path

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build'))));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     });
