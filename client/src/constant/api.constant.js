05873b537e5c7f73ae3fcc83ea83c6549a11d5b7
unknown <vikichet@gmail.com>
2021-06-19 01:43:08 +0530

    made client api call  production ready

--- client/src/constant/api.constant.js
+++ client/src/constant/api.constant.js
@@ -1,12 +1,26 @@
-const api = {
-    'signup' : 'http://localhost:5000/api/v1/auth/signup',
-    'signin' : 'http://localhost:5000/api/v1/auth/signin',
-    'signout' :'http://localhost:5000/api/v1/auth/signout',
-    'currentUser': 'http://localhost:5000/api/v1/user/current',
-    'uploadFile': 'http://localhost:5000/api/v1/article/uploadfile',
-    'image': 'http://localhost:5000/uploads/',
-    'article': 'http://localhost:5000/api/v1/article',
-    'stories': 'http://localhost:5000/api/v1/article/stories'
-}
+
+process.env.NODE_ENV === 'production'
+
+const api =
+    process.env.NODE_ENV === 'production'
+        ? {
+            'signup': '/api/v1/auth/signup',
+            'signin': '/api/v1/auth/signin',
+            'signout': '/api/v1/auth/signout',
+            'currentUser': '/api/v1/user/current',
+            'uploadFile': '/api/v1/article/uploadfile',
+            'image': '/uploads/',
+            'article': '/api/v1/article',
+            'stories': '/api/v1/article/stories'
+        } : {
+            'signup': 'http://localhost:5000/api/v1/auth/signup',
+            'signin': 'http://localhost:5000/api/v1/auth/signin',
+            'signout': 'http://localhost:5000/api/v1/auth/signout',
+            'currentUser': 'http://localhost:5000/api/v1/user/current',
+            'uploadFile': 'http://localhost:5000/api/v1/article/uploadfile',
+            'image': 'http://localhost:5000/uploads/',
+            'article': 'http://localhost:5000/api/v1/article',
+            'stories': 'http://localhost:5000/api/v1/article/stories'
+        }
 
 export default api;
--- client/src/constant/apiV2.constant.js
+++ client/src/constant/apiV2.constant.js
@@ -1,6 +1,11 @@
-const apiV2 = {
-    'uploadFile': 'http://localhost:5000/api/v2/article/uploadfile',
-    'article': 'http://localhost:5000/api/v2/article'
-}
+const apiV2 =
+    process.env.NODE_ENV === 'production' ?
+        {
+            'uploadFile': '/api/v2/article/uploadfile',
+            'article': '/api/v2/article'
+        } : {
+            'uploadFile': 'http://localhost:5000/api/v2/article/uploadfile',
+            'article': 'http://localhost:5000/api/v2/article'
+        }
 
 export default apiV2;
